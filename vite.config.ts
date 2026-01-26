dc4d01e1b3f04785faee42fc774b71375a26da76
lifeadsevent-cmyk <lifeadsevent@gmail.com>
2026-01-26 04:13:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ← LA LIGNE LA PLUS IMPORTANTE
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  },
-  // Supprimez tout le reste temporairement
-});
+  }
+})
