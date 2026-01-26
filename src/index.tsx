967bc8cc6710d686ad5b909dd584a383636846a3
lifeadsevent-cmyk <lifeadsevent@gmail.com>
2026-01-26 03:54:14 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,8 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1 className="text-3xl text-center mt-10 text-green-400">
-      React fonctionne !
-    </h1>
+    <App />
   </React.StrictMode>
 );
